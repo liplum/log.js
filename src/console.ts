a3d0f6510bdba4572b5d2f89a3746692b2d99884
Liplum <Li_plum@outlook.com>
2025-07-21 18:26:55 +0800

    removed chalk

--- src/console.ts
+++ src/console.ts
@@ -1,22 +1,4 @@
-import { LogLevel } from "./level.js"
 import { LoggingTarget, LoggingTargetEventPayload } from "./listener.js"
-import chalk from "chalk"
-
-export type Tinter = (...text: any[]) => string
-
-export type TinterResolver = (level: LogLevel) => Tinter | undefined
-
-export const LogLevels = {
-  ERROR: chalk.bold.red,
-  WARN: chalk.yellow,
-  INFO: chalk.green,
-  DEBUG: chalk.blue,
-  VERBOSE: undefined,
-}
-
-const baseTinterResolver: TinterResolver = (level) => {
-  return LogLevels[level.toLocaleUpperCase()]
-}
 
 export interface ConsoleLogging {
   on: (target: LoggingTarget) => void
@@ -25,10 +7,9 @@ export interface ConsoleLogging {
 
 export const createConsoleLogging = (args?: {
   logLevels?: string[],
-  tinterResolver?: TinterResolver,
 }): ConsoleLogging => {
+  // uppercase all log levels for consistency
   const logLevels = args?.logLevels?.map((level) => level.toLocaleUpperCase())
-  const tinterResolver = args?.tinterResolver || baseTinterResolver
 
   const id2Listener = new Map<string, (payload: LoggingTargetEventPayload) => void>()
   return {
@@ -39,8 +20,19 @@ export const createConsoleLogging = (args?: {
         // If logLevels is specified, only log messages with levels in that array
         if (logLevels && !logLevels.includes(level.toLocaleUpperCase())) return
 
-        const tinter = tinterResolver(level)
-        console.log(tint(message, tinter))
+        if (level === "ERROR") {
+          console.error(message)
+        } else if (level === "WARN") {
+          console.warn(message)
+        } else if (level === "INFO") {
+          console.info(message)
+        } else if (level === "DEBUG") {
+          console.debug(message)
+        } else if (level === "VERBOSE") {
+          console.debug(message)
+        } else {
+          console.log(message)
+        }
       }
       id2Listener.set(target.id, listener)
       target.on("log", listener)
@@ -53,8 +45,4 @@ export const createConsoleLogging = (args?: {
       id2Listener.delete(target.id)
     }
   }
-}
-
-const tint = (text: string, color?: Tinter): string => {
-  return color ? color(text) : text
 }
