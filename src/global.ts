5a4026c000fa4fa5210529e585388bcadd772b69
Liplum <Li_plum@outlook.com>
2025-07-23 13:05:52 +0800

    off the globalConsoleLogging on exit

--- src/global.ts
+++ src/global.ts
@@ -2,6 +2,7 @@ import { createConsoleLogging } from "./console.js"
 import { Logger } from "./logger.js"
 import { LoggerProvider, createLoggerProvider } from "./provider.js"
 import * as jsEnv from "browser-or-node"
+import { onExit } from "signal-exit"
 
 export const globalLoggerProvider: LoggerProvider = createLoggerProvider()
 
@@ -16,3 +17,7 @@ globalConsoleLogging.on(globalLoggerProvider)
 export const createLogger = (channel?: string): Logger => {
   return globalLoggerProvider.createLogger(channel)
 }
+
+onExit(() => {
+  globalConsoleLogging.off(globalLoggerProvider)
+})
