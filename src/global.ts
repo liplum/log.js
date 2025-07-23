8ea24f4d28735274d045719af424c89dc6324218
Liplum <Li_plum@outlook.com>
2025-07-23 13:08:45 +0800

    Revert "off the globalConsoleLogging on exit"
    This reverts commit 5a4026c000fa4fa5210529e585388bcadd772b69.

--- src/global.ts
+++ src/global.ts
@@ -2,7 +2,6 @@ import { createConsoleLogging } from "./console.js"
 import { Logger } from "./logger.js"
 import { LoggerProvider, createLoggerProvider } from "./provider.js"
 import * as jsEnv from "browser-or-node"
-import { onExit } from "signal-exit"
 
 export const globalLoggerProvider: LoggerProvider = createLoggerProvider()
 
@@ -17,7 +16,3 @@ globalConsoleLogging.on(globalLoggerProvider)
 export const createLogger = (channel?: string): Logger => {
   return globalLoggerProvider.createLogger(channel)
 }
-
-onExit(() => {
-  globalConsoleLogging.off(globalLoggerProvider)
-})
